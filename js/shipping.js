c1da95cd63b3c8eadb66f290642c6a02ab112a8f
Todd Austin <toddaustin33@gmail.com>
2017-03-07 15:42:21 -0700

    update signup validation

--- js/shipping.js
+++ js/shipping.js
@@ -1,8 +1,9 @@
 // validation event listeners
 
 var myForm = document.getElementById("shipping");
-
-myForm.addEventListener('input', function(event) {
+var c = 0;
+myForm.addEventListener('submit', function(event) {
+ 
     // stop the event from its default action: submitting the form (for our validation, submission is not desired)
     var fName = document.querySelector('input[name=first-name]');
     var lName = document.querySelector('input[name=last-name]');
@@ -16,6 +17,8 @@ myForm.addEventListener('input', function(event) {
     var cityVal = city.value;
     var countryVal = country.value;
 
+    alert('number '+ c++ + "  " +fVal);
+
     if (!validator.isTrimmed(fVal) || validator.isEmpty(fVal)) {
         fName.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
     } else {
@@ -26,61 +29,61 @@ myForm.addEventListener('input', function(event) {
     } else {
         lName.setCustomValidity("");
     }
-    if (!validator.isTrimmed(aVal) || validator.isEmpty(aVal)) {
-        address.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
-    } else {
-        address.setCustomValidity("");
-    }
-    if (!validator.isTrimmed(cityVal) || validator.isEmpty(cityVal)) {
-        city.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
-    } else {
-        city.setCustomValidity("");
-    }
-    if (!validator.isTrimmed(countryVal) || validator.isEmpty(countryVal)) {
-        country.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
-    } else {
-        country.setCustomValidity("");
-    }
+    // if (!validator.isTrimmed(aVal) || validator.isEmpty(aVal)) {
+    //     address.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    // } else {
+    //     address.setCustomValidity("");
+    // }
+    // if (!validator.isTrimmed(cityVal) || validator.isEmpty(cityVal)) {
+    //     city.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    // } else {
+    //     city.setCustomValidity("");
+    // }
+    // if (!validator.isTrimmed(countryVal) || validator.isEmpty(countryVal)) {
+    //     country.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    // } else {
+    //     country.setCustomValidity("");
+    // }
 
-    var billing = document.querySelector("input[name=same]").checked;
-    if (!billing){
-        var bfName = document.querySelector('input[name=b-first-name]');
-        var blName = document.querySelector('input[name=b-last-name]');
-        var baddress = document.querySelector('input[name=b-address]');
-        var bcity = document.querySelector('input[name=b-city]');
-        var bcountry = document.querySelector('input[name=b-country]');
-        var bfVal = bfName.value;
-        var blVal = blName.value;
-        var baVal = baddress.value;
-        var bcityVal = bcity.value;
-        var bcountryVal = bcountry.value;
+    // var billing = document.querySelector("input[name=same]").checked;
+    // if (!billing){
+    //     var bfName = document.querySelector('input[name=b-first-name]');
+    //     var blName = document.querySelector('input[name=b-last-name]');
+    //     var baddress = document.querySelector('input[name=b-address]');
+    //     var bcity = document.querySelector('input[name=b-city]');
+    //     var bcountry = document.querySelector('input[name=b-country]');
+    //     var bfVal = bfName.value;
+    //     var blVal = blName.value;
+    //     var baVal = baddress.value;
+    //     var bcityVal = bcity.value;
+    //     var bcountryVal = bcountry.value;
 
-        if (!validator.isTrimmed(bfVal) || validator.isEmpty(bfVal)) {
-            bfName.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
-        } else {
-            bfName.setCustomValidity("");
-        }
-        if (!validator.isTrimmed(blVal) || validator.isEmpty(blVal)) {
-            blName.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
-        } else {
-            blName.setCustomValidity("");
-        }
-        if (!validator.isTrimmed(baVal) || validator.isEmpty(baVal)) {
-            baddress.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
-        } else {
-            baddress.setCustomValidity("");
-        }
-        if (!validator.isTrimmed(bcityVal) || validator.isEmpty(bcityVal)) {
-            bcity.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
-        } else {
-            bcity.setCustomValidity("");
-        }
-        if (!validator.isTrimmed(bcountryVal) || validator.isEmpty(bcountryVal)) {
-            bcountry.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
-        } else {
-            bcountry.setCustomValidity("");
-        }
-    }
+    //     if (!validator.isTrimmed(bfVal) || validator.isEmpty(bfVal)) {
+    //         bfName.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    //     } else {
+    //         bfName.setCustomValidity("");
+    //     }
+    //     if (!validator.isTrimmed(blVal) || validator.isEmpty(blVal)) {
+    //         blName.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    //     } else {
+    //         blName.setCustomValidity("");
+    //     }
+    //     if (!validator.isTrimmed(baVal) || validator.isEmpty(baVal)) {
+    //         baddress.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    //     } else {
+    //         baddress.setCustomValidity("");
+    //     }
+    //     if (!validator.isTrimmed(bcityVal) || validator.isEmpty(bcityVal)) {
+    //         bcity.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    //     } else {
+    //         bcity.setCustomValidity("");
+    //     }
+    //     if (!validator.isTrimmed(bcountryVal) || validator.isEmpty(bcountryVal)) {
+    //         bcountry.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    //     } else {
+    //         bcountry.setCustomValidity("");
+    //     }
+    // }
     
     event.preventDefault();
 });
--- js/signup.js
+++ js/signup.js
@@ -1,33 +1,44 @@
-// validation event listeners
 
 var myForm = document.getElementById("signup");
+var fName = document.querySelector('input[name=first-name]');
+var lName = document.querySelector('input[name=last-name]');
+var eMail = document.querySelector('input[name=email]');
+var fName = document.querySelector('input[name=first-name]');
+var lName = document.querySelector('input[name=last-name]');
+var eMail = document.querySelector('input[name=email]');
+var pWord = document.getElementById('password');
 
-myForm.addEventListener('input', function(event) {
-    // stop the event from its default action: submitting the form (for our validation, submission is not desired)
-    var fName = document.querySelector('input[name=first-name]');
-    var lName = document.querySelector('input[name=last-name]');
-    var eMail = document.querySelector('input[name=email]');
-    var fVal = fName.value;
-    var lVal = lName.value;
-    var emailVal = eMail.value;
-
-    if (!validator.isTrimmed(fVal) || validator.isEmpty(fVal)) {
-        fName.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+myForm.addEventListener('submit',isValid);
+myForm.addEventListener('input', isValid);
+  
+function isValid(){
+    if (validator.isEmpty(fName.value)) {
+        fName.setCustomValidity("Please double check your entry, it is either empty");
     } else {
         fName.setCustomValidity("");
     }
-    if (!validator.isTrimmed(lVal) || validator.isEmpty(lVal)) {
-        lName.setCustomValidity("Please double check your entry, it is either empty or has too many spaces");
+    if (validator.isEmpty(lName.value)) {
+        lName.setCustomValidity("Please double check your entry, it is either empty");
     } else {
         lName.setCustomValidity("");
     }
 
-    if (!validator.isTrimmed(emailVal) || validator.isEmpty(emailVal) || !validator.isEmailAddress(emailVal)) {
-        eMail.setCustomValidity("Please double check your entry, it is not a email");
+    if (validator.isEmpty(eMail.value) || !validator.isEmailAddress(eMail.value)) {
+        eMail.setCustomValidity("Please double check your entry, it is not a email or it is empty");
     } else {
         eMail.setCustomValidity("");
     }
 
-    
+    if (validator.isEmpty(pWord.value)) {
+        pWord.setCustomValidity("Please double check your entry, it cannot be empty");
+    } else {
+        pWord.setCustomValidity("");
+    }
+
     event.preventDefault();
-});
+}
+
+
+
+
+
